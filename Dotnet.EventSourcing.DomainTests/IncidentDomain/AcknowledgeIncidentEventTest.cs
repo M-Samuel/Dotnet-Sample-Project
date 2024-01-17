10fa55674013c13a761f41914ef9d46e205422ea
M-Samuel <samuel.m.modeste@gmail.com>
2024-01-17 07:43:04 +0000

    Ack Event tests

--- Dotnet.EventSourcing.DomainTests/IncidentDomain/AcknowledgeIncidentEventTest.cs
+++ Dotnet.EventSourcing.DomainTests/IncidentDomain/AcknowledgeIncidentEventTest.cs
@@ -26,6 +26,62 @@ public class AcknowledgeIncidentEventTest
 
     }
 
+    [Test]
+    public async Task AcknowledgeIncident_FakeIncident_RetursIncidentNotExistsError()
+    {
+        IUserRepository userRepository = new UserExists_FakeUserRepository();
+        IIncidentRepository incidentRepository = new FakeIncident_RetursIncidentNotExistsError_FakeIncidentRepository();
+        IncidentService incidentService = new (incidentRepository, userRepository);
+
+        AcknowledgeIncidentEvent acknowledgeIncidentEvent = new(
+            DateTime.UtcNow,
+            Guid.NewGuid(),
+            Guid.NewGuid()
+        );
+
+        var result = await incidentService.ProcessDomainEvent(acknowledgeIncidentEvent);
+
+        Assert.IsTrue(result.DomainErrors.Any(error => error is IncidentNotFoundError));
+
+    }
+
+    [Test]
+    public async Task AcknowledgeIncident_FakeUser_RetursUserNotFoundError()
+    {
+        IUserRepository userRepository = new UserNotExist_FakeUserRepository();
+        IIncidentRepository incidentRepository = new Correct_RetursNoError_FakeIncidentRepository();
+        IncidentService incidentService = new (incidentRepository, userRepository);
+
+        AcknowledgeIncidentEvent acknowledgeIncidentEvent = new(
+            DateTime.UtcNow,
+            Guid.NewGuid(),
+            Guid.NewGuid()
+        );
+
+        var result = await incidentService.ProcessDomainEvent(acknowledgeIncidentEvent);
+
+        Assert.IsTrue(result.DomainErrors.Any(error => error is UserNotFoundError));
+
+    }
+
+    private class UserNotExist_FakeUserRepository : IUserRepository
+    {
+        public Task CreateUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User?> GetUserByIdAsync(Guid userId)
+        {
+            return await Task.FromResult<User?>(null);
+        }
+
+        public Task<User?> GetUserByNameAsync(string firstName, string lastName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     private class UserExists_FakeUserRepository : IUserRepository
     {
         public Task CreateUserAsync(User user)
@@ -71,6 +127,24 @@ public class AcknowledgeIncidentEventTest
         public async Task UpdateIncidentAsync(Incident incident)
         {
             await Task.CompletedTask;
+        }
+    }
+
+    private class FakeIncident_RetursIncidentNotExistsError_FakeIncidentRepository : IIncidentRepository
+    {
+        public Task CreateIncidentAsync(Incident incident)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Incident?> GetIncidentByIdAsync(Guid incidentId)
+        {
+            return await Task.FromResult<Incident?>(null);
+        }
+
+        public Task UpdateIncidentAsync(Incident incident)
+        {
+            throw new NotImplementedException();
         }
     }
 }
